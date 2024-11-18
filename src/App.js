596923651a6ee7177e92079eaf11c99bff392f4c
paulosena <paulosenamne@gmail.com>
2024-11-18 14:26:47 -0300

    feat: iniciando interface shopfloor

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          vendo para que serve esse lixo de React
         </a>
       </header>
     </div>
